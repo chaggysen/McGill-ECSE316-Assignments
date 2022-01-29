9f50b732e4a990cfe7558fd004b7c85fe9f665f5
chaggysen <sen.wang@mail.mcgill.ca>
2022-01-29 12:48:53 -0500

    initial commit

---
+++ Assignment1/app.py
@@ -0,0 +1,11 @@
+def __main__():
+    print("Hello World")
+
+
+def set_arguments():
+    # TODO
+    pass
+
+
+if __name__ == "__main__":
+    __main__()
---
+++ Assignment1/client_dns.py
@@ -0,0 +1,9 @@
+import socket
+
+
+class DNS_CLIENT:
+    def __init__(self):
+        pass
+
+    def send_query():
+        pass

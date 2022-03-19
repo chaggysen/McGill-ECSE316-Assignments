e61d008bb692a507f072407bf20773c231d18533
Noah Chamberland <59708478+SpaciousFish@users.noreply.github.com>
2022-03-19 17:55:47 -0400

    add calling syntax

---
+++ Assignment2/fft.py
@@ -0,0 +1,21 @@
+import argparse
+from fourier_transform import FOURIER_TRANSFORM
+
+
+def __main__():
+    args = set_arguments()
+    FOURIER_TRANSFORM(args)
+    
+
+def set_arguments():
+    parser = argparse.ArgumentParser(description="Fourier Transform!")
+    parser.add_argument("-m", type=int, help="Mode",
+                        dest="MODE", default=1)
+    parser.add_argument(
+        "-i", help="Image", dest="IMAGE", default='moonlanding.png')
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    __main__()
---
+++ Assignment2/fourier_transform.py
@@ -0,0 +1,4 @@
+class FOURIER_TRANSFORM:
+    def __init__(self, args):
+        self.mode = args.MODE
+        self.image = args.IMAGE
